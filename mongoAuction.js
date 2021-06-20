58896ae8c93b83818149a11bc841833a4d48046d
jguzaa <j_guzaa@hotmail.com>
2021-06-20 23:04:35 +1000

    delete comment

--- mongoAuction.js
+++ mongoAuction.js
@@ -1,6 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
 const MongoObjId = require('mongodb').ObjectId;
-//const uri = "mongodb+srv://jongjet:jongjet25@myapp.mljmt.mongodb.net/auctionit?retryWrites=true&w=majority";
 const uri = "mongodb://localhost:27017/";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
